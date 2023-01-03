b43487cc742a1e76d74ed388ee9753de6652ec6f
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-01-03 21:44:04 +0100

    Saving block test 1

--- test/SavingBlock.ts
+++ test/SavingBlock.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { SavingBlock } from '../typechain-types/contracts/SavingBlock.sol/SavingBlock';
 
 describe("Saving Block Contract", function () {
     
@@ -17,14 +18,19 @@ describe("Saving Block Contract", function () {
         const Token = _token.deploy(initialSupply, tokenName, symbol, decimal);
 
         const _usdt = (await Token).address;
-        const _decimal = (await Token).decimals();
         const _signupFee = 10;      
 
         const _savingBlock = await ethers.getContractFactory("SavingBlock");
 
-        const SavingBlock = _savingBlock.deploy(admin.toString(), _usdt, _decimal, _signupFee)
+        const SavingBlock = _savingBlock.deploy(admin.toString(), _usdt, decimal, _signupFee);
+
+        return { SavingBlock, admin, _usdt, decimal, _signupFee, user1, user2, user3, user4 };
+    }
+
+    it("It should be successful during signup and give referrals", async function() {
+        const [SavingBlock, admin, user1, user2] = await loadFixture(deploySavingBlockFixture);
 
         
-        
-    }
+
+    });
 });

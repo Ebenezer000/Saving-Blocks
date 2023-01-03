ce87a3105d3b69a0858dae350d55d3842759c2de
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-01-03 20:36:48 +0100

    Contact changes

--- test/SavingBlock.ts
+++ test/SavingBlock.ts
@@ -1,3 +1,14 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
+describe("Saving Block Contract", function () {
+    
+    async function deploySavingBlockFixture() {
+
+        const [ admin, user1, user2, user3, user4 ] = await ethers.getSigners();
+
+        const _savingsBlock = ethers.getContractFactory("SavingsBlock")
+        
+    }
+});

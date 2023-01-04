89b410c430000666c998d285caef57fc65b53c47
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-01-04 01:29:39 +0100

    Made Final changes to test script

--- test/SavingBlock.ts
+++ test/SavingBlock.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
-import { SavingBlock, NewUserAddedEvent } from '../typechain-types/contracts/SavingBlock.sol/SavingBlock';
+import { SavingBlock, DepositSuccessfulEvent } from '../typechain-types/contracts/SavingBlock.sol/SavingBlock';
 
 describe("Saving Block Contract", function () {
     
@@ -32,9 +32,51 @@ describe("Saving Block Contract", function () {
     it("It should be successful during signup and give referrals", async function() {
         const { SavingBlock, user1, user2 }  = await loadFixture(deploySavingBlockFixture);
         
-        await expect(
+        expect(
             await SavingBlock.connect(user1).SignUp(user2.toString())
-        ).to.emit(SavingBlock, "NewUserAdded");
+        ).to.true;
 
+    });
+
+    it("It should recieve savings and take away referral bonus", async function() {
+        const {SavingBlock, user1, user2 } = await loadFixture(deploySavingBlockFixture);
+
+        expect(
+            (await SavingBlock.connect(user1).Save(50))
+        ).to.true;
+    });
+
+    it("It should complete a lending transaction with only referrals as payback", async function() {
+        const { SavingBlock, user1 } = await loadFixture(deploySavingBlockFixture);
+
+        expect(
+            await SavingBlock.connect(user1).LendWithReferrals(50, 100)
+        ).to.true;
+    });
+
+    it("it should complete a referrals transaction with guarantors as payback", async function() {
+        const { SavingBlock, user1, user2, user3, user4 } = await loadFixture(deploySavingBlockFixture);
+
+        const ONE_DAY_IN_SECS = 24 * 60 * 60;
+        expect(
+            await SavingBlock.connect(user1).LendWithGuarrantors(
+                    90, [user2.toString(), user3.toString(), user4.toString()], ONE_DAY_IN_SECS)
+        ).to.true;
+    });
+
+    it ("It should Allow user withdraw successfully", async function () {
+        const { SavingBlock, user1 } = await loadFixture(deploySavingBlockFixture);
+
+        expect(
+            await SavingBlock.connect(user1).UserWithdraw(40)
+        ).to.true;
+    });
+
+    it ("It should allow Admin to withdraw successfully", async function() {
+        const { SavingBlock, user1 } = await loadFixture(deploySavingBlockFixture);
+
+        expect(
+            await SavingBlock.connect(user1).AdminWithdraw(50)
+        ).to.true;
     });
 });

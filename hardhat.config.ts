56cb375fe6fda032855e812b172fb86bc5a0d2c2
Ebenezer000 <79841424+Ebenezer000@users.noreply.github.com>
2023-01-10 17:37:36 +0100

    Update git

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,44 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-ethers';
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+const { mnemonic } = require('./secrets.json');
+
+module.exports = {
+  defaultNetwork: "testnet",
+  networks: {
+  	localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  solidity: {
+  version: "0.8.17",
+  settings: {
+    optimizer: {
+      enabled: true
+    }
+   }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
-
-export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const admin = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const _usdt = "0x60F81573976c62313484B0406ff28c685Eb9c48F";
+  const _signupFee = "10";      
+  const decimal = "6";
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const _savingBlock = await ethers.getContractFactory("SavingBlock");
 
-  await lock.deployed();
+  const SavingBlock = await _savingBlock.deploy(admin.toString(), _usdt, decimal, _signupFee);
+  await SavingBlock.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Saving Block has been deployed with address ${SavingBlock.address} and admin ${admin} `);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
